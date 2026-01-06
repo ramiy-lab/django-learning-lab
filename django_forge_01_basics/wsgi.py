1fa2a9544e88504e92cb1d08c4b932c8e5125032
ramiy-star <heitianlu.01@outlook.jp>
2026-01-06 16:06:49 +0900

    Set up pages app with services layer and initial MVT structure

--- django_forge_01_basics/wsgi.py
+++ django_forge_01_basics/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_forge_01_basics.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_forge_01_basics.settings")
 
 application = get_wsgi_application()
