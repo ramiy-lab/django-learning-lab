1430cb366debc6a6f55a35c6308e612586239d6b
ramiy-star <heitianlu.01@outlook.jp>
2026-01-17 21:49:14 +0900

    Add verbose_name to model fields for readabillity

--- pages/models.py
+++ pages/models.py
@@ -1,17 +1,28 @@
+from email.policy import default
 from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField()
+    title = models.CharField(
+        max_length=100,
+        verbose_name="タイトル",
+    )
 
     subtitle = models.CharField(
         max_length=100,
-        null=True,
-        blank=True
+        blank=True,
+        default="",
+        verbose_name="サブタイトル",
     )
 
-    view_count = models.IntegerField()
+    body = models.TextField(
+        verbose_name="本文",
+    )
+
+    view_count = models.IntegerField(
+        default=0,
+        verbose_name="閲覧数",
+    )
 
     def __str__(self):
         return self.title
