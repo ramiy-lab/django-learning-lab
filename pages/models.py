86c74af4aebf5c7ea0fcdfb716a098cf777d6c5b
ramiy-star <heitianlu.01@outlook.jp>
2026-01-17 21:27:07 +0900

    Clarify null and blank responsibilities in model fields

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 
-class Article(models.Model):  # type: ignore[misc]
+class Article(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    views = models.IntegerField()
+
+    subtitle = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    view_count = models.IntegerField()
 
     def __str__(self):
         return self.title
