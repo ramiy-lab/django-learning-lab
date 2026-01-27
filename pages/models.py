18cfcf8fd76bef45eb68dc990a5be895dce5f647
ramiy-star <heitianlu.01@outlook.jp>
2026-01-27 14:41:06 +0900

    Add Author and rebuild dataset for ForeignKey practice

--- pages/models.py
+++ pages/models.py
@@ -2,26 +2,27 @@ from email.policy import default
 from django.db import models
 
 
+class Author(models.Model):  # type: ignore[misc]
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
 class Article(models.Model):  # type: ignore[misc]
     title = models.CharField(
         max_length=100,
         verbose_name="タイトル",
     )
 
-    subtitle = models.CharField(
-        max_length=100,
-        blank=True,
-        default="",
-        verbose_name="サブタイトル",
-    )
-
     body = models.TextField(
         verbose_name="本文",
     )
 
-    view_count = models.IntegerField(
-        default=0,
-        verbose_name="閲覧数",
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="articles",
     )
 
     class Meta:
